4a70b013d0914403a3c5cda24897c50b56ae207f
Tumas Bajoras <hanniph@gmail.com>
2010-03-21 22:09:08 +0200

    created guessing context in word_spec

--- IV_semester/ruby/learning_app/spec/learning_app/word_spec.rb
+++ IV_semester/ruby/learning_app/spec/learning_app/word_spec.rb
@@ -26,6 +26,37 @@ module LearningSystem
         end
       end
 
+      context "guessing " do
+        before(:each) do
+          @word = Word.new("Test", "form of examination", "similar to quiz")
+          @correct_guesses = ["Test", "TEST", "test", " test ", "test ", " test"]
+        end
+
+        it "should return true if guess was correct" do
+          @correct_guesses.each do |answer|
+            word.guess_value(answer).should == true
+          end
+        end
+
+        it "should return false if guess was incorrect" do
+          @word.guess_value("quiz").should == false
+        end
+
+        # #{correct_guesses} does not work
+        it "should be guessed and answerred ? times" do
+          @correct_guesses.each do |answer|
+            word.guess_value(answer)
+          end
+          word.times_guessed.should == @correct_guesses.size
+          word.times_answered.should == @correct_guesses.size 
+        end
+
+        it "should be guessed 2 times and answerred 0 times" do
+          2.times { @word.guess_value("quiz") }
+          word.times_guessed.should == 2
+          word.times_answered.should == 0
+        end
+      end
     end
   end
 end
