2e2b7fa5d11600c03eb2d36ccf272e36008dabf8
Tumas Bajoras <hanniph@gmail.com>
2010-04-05 19:02:32 +0300

    gui updates

--- IV_semester/ruby/learning_app/bin/add_quiz_widget.rb
+++ IV_semester/ruby/learning_app/bin/add_quiz_widget.rb
@@ -5,43 +5,61 @@ class Quizzer < Shoes::Widget
     @user = opts[:user]
     @title = opts[:title]
     @mode = opts[:mode]
-    @quiz = if opts[:quiz].nil?
-      Quiz.new('name of the quiz') 
+
+    # subject - test/quiz
+    # subject_object - test or quiz object
+    # subjects - plural or subject tests/quizzes
+
+    @subject = opts[:subject]
+    @subject = 'quiz' if @subject.nil?
+
+    if @subject == 'quiz'
+      if opts[:quiz].nil?  
+        @subject_object = Quiz.new('name of the quiz') 
+      else
+        @subject_object = opts[:quiz]
+      end
+      @subjects = 'quizzes'
     else
-      opts[:quiz]
+      if opts[:test].nil?
+        @subject_object = Test.new('name of the test')
+      else
+        @subject_object = opts[:test]
+      end
+      @subjects = 'tests'
     end
 
     @final_text = if @mode == 'add'
-      'add quiz'
+      "add #{@subject}"
     elsif @mode == 'edit'
-       'update quiz'
+       "update #{@subject}"
     end
 
-    @quiz_words = []
+    @subject_words = []
     @user_words = []
 
     stack :margin_top => 10 do
       subtitle @title, :stroke => white, :align => "center"
 
       stack :top => 80, :left => 160, :align => "center" do
-        @name = edit_line :width => 400, :text => @quiz.name 
+        @name = edit_line :width => 400, :text => @subject_object.name 
       end
 
       stack :top => 200, :left => 20 do
-        para(link("oh, nervermind..", :click => "/quizzes"), :top => 370, :left => 310)
+        para(link("oh, nervermind..", :click => "/#{@subjects}"), :top => 370, :left => 310)
 
         @user.each_word {|w| @user_words << [w.value] }
-        update_quiz_words_list
+        update_subject_words_list
 
-        @wt = table(:top => -80, :left => 130, :rows => 5, :headers => [["User words", 150],], :items => @user_words, :blk => nil)
-        @qw = table(:top => -80, :left => 400, :rows => 5, :headers => [["Quiz words", 150],], :items => @quiz_words, :blk => nil)
+        @wt = table(:top => -80, :left => 130, :rows => 5, :headers => [["user words", 150],], :items => @user_words, :blk => nil)
+        @qw = table(:top => -80, :left => 400, :rows => 5, :headers => [["#{@subject} words", 150],], :items => @subject_words, :blk => nil)
 
         button "-->", :top => -30, :left => 320 do
           unless @wt.selected.nil?
             begin
-              @quiz.add_word(@user.word({ :value => @user_words[@wt.selected][0] }))
-              update_quiz_words_list
-              @qw.update_items(@quiz_words)
+              @subject_object.add_word(@user.word({ :value => @user_words[@wt.selected][0] }))
+              update_subject_words_list
+              @qw.update_items(@subject_words)
             rescue Exception => msg
               alert msg
             end
@@ -49,21 +67,33 @@ class Quizzer < Shoes::Widget
         end
 
         button "<--", :top => 0, :left => 320 do
-          @quiz.remove_word(@user.word({ :value => @quiz_words[@qw.selected][0]}))
-          update_quiz_words_list
-          @qw.update_items(@quiz_words)
+          @subject_object.remove_word(@user.word({ :value => @subject_words[@qw.selected][0]}))
+          update_subject_words_list
+          @qw.update_items(@subject_words)
         end
 
         button @final_text, :margin_left => 300, :margin_top => 130, :margin_bottom => 10  do
           begin
-            @quiz.name = @name.text
-            @user.add_quiz(@quiz, true) if @mode == 'edit'
+            @subject_object.name = @name.text
+            if @mode == 'edit'
+              if @subject == 'quiz' 
+                @user.add_quiz(@subject_object, true) 
+              else
+                @user.add_test(@subject_object, true)
+              end
+            else
+              if @subject == 'quiz' 
+                @user.add_quiz(@subject_object) 
+              else
+                @user.add_test(@subject_object)
+              end
+            end
 
             LearningSystemShoesScript.save_state
             mes = 'added'
             mes = 'updated' if @mode == 'edit'
-            alert "Quiz #{@name.text} was succesfully #{mes}"
-            visit '/quizzes'
+            alert "#{@subject} #{@name.text} was succesfully #{mes}"
+            visit "/#{@subjects}" 
           rescue Exception => msg
             alert msg
           end
@@ -73,8 +103,8 @@ class Quizzer < Shoes::Widget
   end
   
   # ------------------------------ 
-  def update_quiz_words_list
-    @quiz_words = []
-    @quiz.each_word {|w| @quiz_words << [w.value] }
+  def update_subject_words_list
+    @subjects_words = []
+    @subject_object.each_word {|w| @subject_words << [w.value] }
   end
 end
