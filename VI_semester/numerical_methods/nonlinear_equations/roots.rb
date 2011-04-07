3da597fff51588966538280afca2125bee7e058c
Tumas Bajoras <hanniph@gmail.com>
2011-04-07 09:47:17 +0300

    hotfixes to root finding algorithms

--- VI_semester/numerical_methods/nonlinear_equations/roots.rb
+++ VI_semester/numerical_methods/nonlinear_equations/roots.rb
@@ -23,9 +23,11 @@ class SIMethod
   def initialize(f, x0, q, epsilon = 0.000001)
     @f = f
     @x0, @x1 = x0.to_f, nil
-    @epsilon, @delta = epsilon.to_f, (1 - q)/q.to_f
+    @epsilon = epsilon.to_f
+    @delta = (1 - q.abs) / q.abs.to_f 
 
     puts "Warning: solution may diverge: #{q}" if q >= 1
+    puts delta
   end
 
   def solve(max_iterations = MAX_ITERATIONS_COUNT)
@@ -65,13 +67,13 @@ class SecantMethod
     iterations = 0
 
     loop do
-      yield @x2, @x1, @x0, iterations if block_given?
-
-      iterations += 1
       val = @f.value(@x1)
       denom = (val - @f.value(@x0))
 
       unless denom.zero?
+        yield @x2, @x1, @x0, iterations if block_given?
+
+        iterations += 1
         @x2 = @x1 - val * (@x1 - @x0) / denom 
       else
         puts "function values are the same. Adjusting first x by 0.1"
@@ -94,7 +96,7 @@ class SecantMethod
   end
 end
 
-sim = SIMethod.new(Function.new("cos(x)"), 0.2, 1)
+sim = SIMethod.new(Function.new("cos(x)"), 0.5, 0.8)
 x = sim.solve do |x1, x0, i|
   msg = "Iteration: #{i}, x#{i}: #{x1}"
   msg += ", |x#{i} - x#{i-1}|: #{(x1-x0).abs}" unless x1.nil?
@@ -112,7 +114,7 @@ puts "Solution: #{x}"
 puts "*" * 20 
 
 #sm = SecantMethod.new(Function.new("atan(x + 1) / 5 - x"), 0, 0.1)
-sm = SecantMethod.new(Function.new("cos(x) - x"), 0, 1)
+sm = SecantMethod.new(Function.new("cos(x) - x"), -2, -2.05)
 x = sm.solve do |x2, x1, x0, i|
   puts "Iteration: #{i}, x: #{x2}, f: #{sm.f.value(x2)}"
 end
