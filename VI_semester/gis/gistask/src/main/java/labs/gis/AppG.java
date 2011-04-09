84bcbf7108c01ab99262fb9a12f078fd122828a8
Tumas Bajoras <hanniph@gmail.com>
2011-04-09 23:15:07 +0300

    Adding multiple layer selection coloring

--- VI_semester/gis/gistask/src/main/java/labs/gis/AppG.java
+++ VI_semester/gis/gistask/src/main/java/labs/gis/AppG.java
@@ -3,7 +3,6 @@ package labs.gis;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
@@ -35,8 +34,11 @@ import org.geotools.styling.Fill;
 import org.geotools.styling.Graphic;
 import org.geotools.styling.Mark;
 import org.geotools.styling.Rule;
+import org.geotools.styling.Stroke;
 import org.geotools.styling.Style;
+import org.geotools.styling.StyleFactory;
 import org.geotools.styling.Symbolizer;
+
 import org.geotools.swing.JMapFrame;
 import org.geotools.swing.action.SafeAction;
 import org.geotools.swing.data.JFileDataStoreChooser;
@@ -44,10 +46,15 @@ import org.geotools.swing.event.MapMouseEvent;
 import org.geotools.swing.tool.CursorTool;
 import org.opengis.feature.Feature;
 import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.GeometryDescriptor;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
 import org.opengis.filter.identity.FeatureId;
-import org.opengis.style.StyleFactory;
+
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
 
 /**
  * [IMPLEMENTED] 
@@ -87,14 +94,25 @@ public class AppG extends JFrame
     private FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
     private StyleFactory sf = CommonFactoryFinder.getStyleFactory(null);
     
+    private enum GeomType { POINT, LINE, POLYGON };
+    
 	private static final Color SELECTED_COLOUR = Color.YELLOW;
+	private static final Color LINE_COLOUR = Color.BLACK;
+    private static final Color FILL_COLOUR = Color.WHITE;
+    private static final Color SELECTED_LINE_COLOUR = Color.ORANGE;
+    
+    private static final float OPACITY = 1.0f;
+    private static final float LINE_WIDTH = 1.0f;
+    private static final float POINT_SIZE = 10.0f;
+    
 	
 	// Enable selection
 	public class ClickCursorTool extends CursorTool {
 		public void onMouseClicked(MapMouseEvent event){
 			selectedFeatures = selectFeatures(event);
-	        System.out.println(selectedFeatures);
-	        //displayFeatures(selectedFeatures);
+	        
+			System.out.println(selectedFeatures);
+	        displayFeatures(selectedFeatures);
 		}
 	}
 
@@ -145,45 +163,44 @@ public class AppG extends JFrame
     	new AppG().start();
     }
 
-    public void displaySelectedFeatures(Set<FeatureId> IDs) {
-    	// Create style for each selected feature
+    public void displayFeatures(Set<FeatureId> IDs) {
+    	Style style;
     	
-    	Style style = createSelectedStyle(IDs);
-        frame.getMapContext().getLayer(0).setStyle(style);
-        frame.getMapPane().repaint();
+    	for (MapLayer m : frame.getMapContext().getLayers()){
+    		// for each layer -> 
+    		// 	create rule for that layer for selected ids
+    		style = createSelectedStyle(m, IDs);
+    		m.setStyle(style);
+    	}
+    	
+    	frame.getMapPane().repaint();
     }
     
-    private Style createSelectedStyle(Set<FeatureId> IDs) {
-        Rule selectedRule = createRule(SELECTED_COLOUR, SELECTED_COLOUR);
-        selectedRule.setFilter(ff.id(IDs));
-
-        Rule otherRule = createRule(Color.black, SELECTED_COLOUR);
+    private Style createSelectedStyle(MapLayer m, Set<FeatureId> IDs) {
+    	Rule selectedRule = createRule(m, SELECTED_LINE_COLOUR, SELECTED_COLOUR);
+    	selectedRule.setFilter(ff.id(IDs));
+    		
+        Rule otherRule = createRule(m, LINE_COLOUR, FILL_COLOUR);
         otherRule.setElseFilter(true);
-
-        FeatureTypeStyle fts = ((org.geotools.styling.StyleFactory) sf).createFeatureTypeStyle();
+            
+        FeatureTypeStyle fts = sf.createFeatureTypeStyle();
         fts.rules().add(selectedRule);
         fts.rules().add(otherRule);
-
-        Style style = ((org.geotools.styling.StyleFactory) sf).createStyle();
+    	
+        Style style = sf.createStyle();
         style.featureTypeStyles().add(fts);
         return style;
     }
     
-    private static final float OPACITY = 1.0f;
-    private static final float LINE_WIDTH = 1.0f;
-    private static final float POINT_SIZE = 10.0f;
-    private enum GeomType { POINT, LINE, POLYGON };
-    
-    private Rule createRule(Color outlineColor, Color fillColor) {
+    private Rule createRule(MapLayer layer, Color outlineColor, Color fillColor) {
         Symbolizer symbolizer = null;
         Fill fill = null;
-        Stroke stroke = (java.awt.Stroke) ((org.geotools.styling.StyleFactory) sf).createStroke(ff.literal(outlineColor), ff.literal(LINE_WIDTH));
-
-        fill = ((org.geotools.styling.StyleFactory) sf).createFill(ff.literal(fillColor), ff.literal(OPACITY));
-        symbolizer = ((org.geotools.styling.StyleFactory) sf).createPolygonSymbolizer();
-       
-        /*
-        switch (GEO) {
+        Stroke stroke = sf.createStroke(ff.literal(outlineColor), ff.literal(LINE_WIDTH));
+        
+        FeatureType schema = layer.getFeatureSource().getSchema();
+		String geometryAttributeName = schema.getGeometryDescriptor().getLocalName();
+        
+        switch (getGeomType(layer)) {
             case POLYGON:
                 fill = ((org.geotools.styling.StyleFactory) sf).createFill(ff.literal(fillColor), ff.literal(OPACITY));
                 symbolizer = sf.createPolygonSymbolizer(stroke, fill, geometryAttributeName);
@@ -192,27 +209,44 @@ public class AppG extends JFrame
                 symbolizer = sf.createLineSymbolizer(stroke, geometryAttributeName);
                 break;
             case POINT:
-                fill = ((org.geotools.styling.StyleFactory) sf).createFill(ff.literal(fillColor), ff.literal(OPACITY));
+                fill = sf.createFill(ff.literal(fillColor), ff.literal(OPACITY));
 
-                Mark mark = ((org.geotools.styling.StyleFactory) sf).getCircleMark();
+                Mark mark = sf.getCircleMark();
                 mark.setFill(fill);
-                mark.setStroke((org.opengis.style.Stroke) stroke);
+                mark.setStroke(stroke);
 
-                Graphic graphic = ((org.geotools.styling.StyleFactory) sf).createDefaultGraphic();
+                Graphic graphic = sf.createDefaultGraphic();
                 graphic.graphicalSymbols().clear();
                 graphic.graphicalSymbols().add(mark);
                 graphic.setSize(ff.literal(POINT_SIZE));
 
                 symbolizer = sf.createPointSymbolizer(graphic, geometryAttributeName);
         }
-        */
 
-        Rule rule = ((org.geotools.styling.StyleFactory) sf).createRule();
+        Rule rule = sf.createRule();
         rule.symbolizers().add(symbolizer);
         return rule;
     }
     
-    public void loadLayer() throws Exception {
+    private GeomType getGeomType(MapLayer layer) {
+    	 GeometryDescriptor geomDesc = layer.getFeatureSource().getSchema().getGeometryDescriptor();
+         Class<?> clazz = geomDesc.getType().getBinding();
+
+         if (Polygon.class.isAssignableFrom(clazz) ||
+                 MultiPolygon.class.isAssignableFrom(clazz)) {
+             return GeomType.POLYGON;
+
+         } else if (LineString.class.isAssignableFrom(clazz) ||
+                 MultiLineString.class.isAssignableFrom(clazz)) {
+
+             return GeomType.LINE;
+
+         } else {
+        	 return GeomType.POINT;
+         }
+	}
+
+	public void loadLayer() throws Exception {
     	File file = JFileDataStoreChooser.showOpenFile("shp", new File("resources/shp"), null);
     	if (file == null) {
             return;
@@ -239,37 +273,34 @@ public class AppG extends JFrame
         Set<FeatureId> IDs = new HashSet<FeatureId>();
 
         for (MapLayer ml : map.getLayers()){
-        	FeatureType schema = ml.getFeatureSource().getSchema();
-        	String geometryAttributeName = schema.getGeometryDescriptor().getLocalName();
-            Filter filter = ff.intersects(ff.property(geometryAttributeName), ff.literal(bbox));
+        	if (ml.isSelected()){
+        		FeatureType schema = ml.getFeatureSource().getSchema();
+        		String geometryAttributeName = schema.getGeometryDescriptor().getLocalName();
+        		Filter filter = ff.intersects(ff.property(geometryAttributeName), ff.literal(bbox));
         	
-        	try {
-        		FeatureCollection selectedFeatures = ml.getFeatureSource().getFeatures(filter);
-        		FeatureIterator iter = selectedFeatures.features();
-
         		try {
-        			while (iter.hasNext()) {
-        				Feature feature = iter.next();
-        				IDs.add(feature.getIdentifier());
+        			FeatureCollection selectedFeatures = ml.getFeatureSource().getFeatures(filter);
+        			FeatureIterator iter = selectedFeatures.features();
+
+        			try {
+        				while (iter.hasNext()) {
+        					Feature feature = iter.next();
+        					IDs.add(feature.getIdentifier());
+        				}
+        			} finally {
+        				iter.close();
         			}
-        		} finally {
-        			iter.close();
-        		}
-            
-        	} catch (Exception ex) {}
+        		} catch (Exception ex) {}
+        	}
         }
-        
         return IDs;
     }
     
 	public void displayShapeFile(File file) throws Exception {
     	FileDataStore store = FileDataStoreFinder.getDataStore(file);
     	FeatureSource featureSource = store.getFeatureSource();
-    	
-    	// FeatureSource featureSource = store.getFeatureSource();
-    	// setGeometry(featureSource);
-    	
     	CachingFeatureSource cache = new CachingFeatureSource(featureSource);
+    	
     	map.addLayer(featureSource, null);
     }
 }
