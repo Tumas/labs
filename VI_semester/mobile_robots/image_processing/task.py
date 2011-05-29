d109647f045cad0b80722d05ae64421ec85e7dc6
Tumas Bajoras <hanniph@gmail.com>
2011-05-29 18:20:38 +0300

    histogram equalization

--- VI_semester/mobile_robots/image_processing/task.py
+++ VI_semester/mobile_robots/image_processing/task.py
@@ -3,9 +3,7 @@
 import matplotlib.pyplot as plt
 import Image
 import ImageFilter
-
-source = "akis.tiff"
-#source = "test.jpg"
+import sys
 
 # pritaikyti vidurkinimo, sobelio operatorius. 
 # Atlikti histogramos normalizavima
@@ -16,29 +14,44 @@ source = "akis.tiff"
     involve weighted combinations of pixels in small neighborhoods.
 """
 
-class _SOBEL(ImageFilter.BuiltinFilter):
-    name = "Sobel"
-    filterargs = (3,3), 0, 0, (-1, -2, -1, 0, 0, 0, +1, +2, +1)
+class _SOBELX(ImageFilter.BuiltinFilter):
+    name = "Sobel_X"
+    filterargs = (3,3), 0, 0, (-1, 0, 1, -2, 0, 2, -1, 0, 1)
+
+class _SOBELY(ImageFilter.BuiltinFilter):
+    name = "Sobel_Y"
+    filterargs = (3,3), 0, 0, (1, 2, 1, 0, 0, 0, -1, -2, -1)
 
 class _AVERAGE(ImageFilter.BuiltinFilter):
     name = "Averaging"
     filterargs = (3,3), 16, 0, (1, 2, 1, 2, 4, 2, 1, 2, 1)
 
-def equalize(h, x, y):
+""" Histogram equalization """
+
+def equalize(h, size):
     """ 
       histogram equalization.
+      h - histogram of an image
     """
-    xy, table = x*y, []
-    cdf = reduce(cdf_func, h, [0])
-    min_cdf = min(cdf)
+    xy, table = im.size[0]*im.size[1], []
 
-    for band in range(0, len(h), 256):
-        for i in range(256):
-            table.append(int(round((cdf[i] - min_cdf) * 255 / (xy - min_cdf))))
+    cdf = reduce(cdf_func, h, [])
+    min_cdf = min(cdf)
+    
+    band_width = 256
+    for band in range(0, len(h), band_width):
+        for i in range(band_width):
+            val = cdf[i] 
+            table.append(round( (val - min_cdf) * 255 / (xy - min_cdf)) )
+
     return table
 
 def cdf_func(l, el):
-    value = el + l[-1]
+    if len(l) > 0: 
+        value = el + l[-1]
+    else:
+        value = el
+
     l.append(value)
     return l
 
@@ -46,19 +59,22 @@ def plot_histogram(h, h1):
     plt.plot(h, 'b-', h1, 'r-')
 
 def equalize_test(im):
-    x, y = im.size
     h = im.histogram()
 
-    lut = equalize(im.histogram(), x, y)
-
+    lut = equalize(im.histogram(), im.size)
     im = im.point(lut)
+
     plot_histogram(h, im.histogram())
+
     plt.show()
     im.show()
 
 if __name__ == '__main__':
-    im = Image.open(source)
+    im = Image.open(sys.argv[1])
+
     #im = im.filter(_AVERAGE)
-    im = im.filter(_SOBEL)
-    #equalize_test(im)
+    #im = im.filter(_SOBELX)
+    #im = im.filter(_SOBELX)
+
     im.show()
+    equalize_test(im)
